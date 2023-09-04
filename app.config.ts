ba61d10cdd4b9cc943421712d7e0643360bc9dec
Gaurabh Chakraborty <103099666+gaurabhgeekyants@users.noreply.github.com>
2023-09-05 03:15:56 +0530

    test commit

--- app.config.ts
+++ app.config.ts
@@ -1,8 +1,8 @@
 export default defineAppConfig({
   docus: {
-    title: 'Docus',
+    title: 'Shopforfit',
     description: 'The best place to start your documentation.',
-    image: 'https://user-images.githubusercontent.com/904724/185365452-87b7ca7b-6030-4813-a2db-5e65c785bf88.png',
+    image: '/cover.png',
     socials: {
       twitter: 'nuxt_js',
       github: 'nuxt-themes/docus',
